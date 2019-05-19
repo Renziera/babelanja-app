9d50bcd892fa74483efb8f91d65dc1721633fe39
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-05-19 12:06:20 +0700

    Kategori Fragment

--- app/src/main/java/id/babelanja/BerandaFragment.java
+++ app/src/main/java/id/babelanja/BerandaFragment.java
@@ -23,10 +23,6 @@ import timber.log.Timber;
 
 public class BerandaFragment extends Fragment {
 
-    EditText tv_search;
-    ImageButton button_menu;
-    ImageButton button_notifikasi;
-
     CarouselView carouselView;
 
     public BerandaFragment() {
@@ -40,12 +36,12 @@ public class BerandaFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_beranda, container, false);
 
-        button_menu = view.findViewById(R.id.button_menu);
+        ImageButton button_menu = view.findViewById(R.id.button_menu);
         button_menu.setOnClickListener(v -> {
             ((MainActivity) Objects.requireNonNull(getActivity())).drawer.openDrawer(GravityCompat.START);
         });
         //TODO https://github.com/nex3z/NotificationBadge
-        button_notifikasi = view.findViewById(R.id.button_notifikasi);
+        ImageButton button_notifikasi = view.findViewById(R.id.button_notifikasi);
         button_notifikasi.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), NotifikasiActivity.class);
             startActivity(intent);
@@ -61,7 +57,7 @@ public class BerandaFragment extends Fragment {
         view.findViewById(R.id.button_pasang_iklan)
                 .setOnClickListener(v -> Toast.makeText(getActivity(), "Belum tersedia", Toast.LENGTH_SHORT).show());
 
-        tv_search = view.findViewById(R.id.tv_search);
+        EditText tv_search = view.findViewById(R.id.tv_search);
         tv_search.setOnEditorActionListener((v, actionId, event) -> {
             if(actionId == EditorInfo.IME_ACTION_SEARCH){
                 Timber.d("Search called");
--- app/src/main/java/id/babelanja/KategoriFragment.java
+++ app/src/main/java/id/babelanja/KategoriFragment.java
@@ -1,13 +1,24 @@
 package id.babelanja;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.ToggleButton;
+
+import java.util.Objects;
+
+import timber.log.Timber;
 
 public class KategoriFragment extends Fragment {
 
@@ -22,6 +33,41 @@ public class KategoriFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_kategori, container, false);
+
+        TableLayout table_barang = view.findViewById(R.id.table_barang);
+        TableLayout table_jasa = view.findViewById(R.id.table_jasa);
+        table_jasa.setVisibility(View.GONE);
+
+        ToggleButton toogle = view.findViewById(R.id.toggle);
+        toogle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked){
+                table_jasa.setVisibility(View.VISIBLE);
+                table_barang.setVisibility(View.GONE);
+            }else {
+                table_barang.setVisibility(View.VISIBLE);
+                table_jasa.setVisibility(View.GONE);
+            }
+        });
+
+        ImageButton button_menu = view.findViewById(R.id.button_menu);
+        button_menu.setOnClickListener(v -> {
+            ((MainActivity) Objects.requireNonNull(getActivity())).drawer.openDrawer(GravityCompat.START);
+        });
+
+        ImageButton button_notifikasi = view.findViewById(R.id.button_notifikasi);
+        button_notifikasi.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), NotifikasiActivity.class);
+            startActivity(intent);
+        });
+
+        EditText tv_search = view.findViewById(R.id.tv_search);
+        tv_search.setOnEditorActionListener((v, actionId, event) -> {
+            if(actionId == EditorInfo.IME_ACTION_SEARCH){
+                Timber.d("Search called");
+                return true;
+            }
+            return false;
+        });
 
         return view;
     }
